870a73eb274bc78be80b916932bf83b5d27b165b
Melody Belle <melody@meluhdy.dev>
2026-04-29 17:31:35 -0400

    Fixed an issue where it only displayed join messages, as well as fixed an issue with locales upon joining

--- src/main/kotlin/dev/meluhdy/scovilleChat/listener/MessageListener.kt
+++ src/main/kotlin/dev/meluhdy/scovilleChat/listener/MessageListener.kt
@@ -5,19 +5,24 @@ import dev.meluhdy.melodia.utils.fromMiniMessage
 import dev.meluhdy.scovilleChat.ScovilleChat
 import dev.meluhdy.scovilleChat.core.player.PlayerMessageSettings
 import dev.meluhdy.scovilleChat.core.player.PlayerMessageSettingsManager
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerLocaleChangeEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import java.util.UUID
 
 object MessageListener : Listener {
 
     fun sendConnectionMessage(p: Player, state: PlayerMessageSettings.ConnectionState) {
-        val playerSettings = PlayerMessageSettingsManager.getOrCreate(p)
-        TextUtils.broadcastChat(ScovilleChat.plugin, playerSettings.connectionMessage.getTranslationString(
-            PlayerMessageSettings.ConnectionState.JOIN), p.name)
+        Bukkit.getScheduler().scheduleSyncDelayedTask(ScovilleChat.plugin, {
+            val playerSettings = PlayerMessageSettingsManager.getOrCreate(p)
+            TextUtils.broadcastChat(ScovilleChat.plugin, playerSettings.connectionMessage.getTranslationString(
+                state), p.name)
+        }, 5L)
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
