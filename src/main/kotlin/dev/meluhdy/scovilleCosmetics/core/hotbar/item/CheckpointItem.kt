067baa567dcc89c243838c1d49db8120badf197c
Melody Belle <melody@meluhdy.dev>
2026-05-11 08:15:44 -0400

    Delegated the CP logic to a helper function

--- src/main/kotlin/dev/meluhdy/scovilleCosmetics/core/hotbar/item/CheckpointItem.kt
+++ src/main/kotlin/dev/meluhdy/scovilleCosmetics/core/hotbar/item/CheckpointItem.kt
@@ -1,14 +1,22 @@
 package dev.meluhdy.scovilleCosmetics.core.hotbar.item
 
+import dev.meluhdy.melodia.utils.TextUtils
+import dev.meluhdy.scoville.Scoville
+import dev.meluhdy.scoville.core.parkourer.ParkourerManager
 import dev.meluhdy.scovilleCosmetics.core.hotbar.ScovilleHotbarItem
-import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
 class CheckpointItem(p: Player, item: ItemStack, color: Char) : ScovilleHotbarItem(p, item, color, "item.hotbar.checkpoint.title") {
 
     override fun onClick(p: Player) {
-        p.sendMessage("Checkpoint")
+        val parkourer = ParkourerManager.get(p) ?: return
+        val course = parkourer.getPlayingCourse()
+        if (course == null) {
+            p.sendMessage(TextUtils.translate(Scoville.plugin, "chat.checkpoint.not_playing", p.locale()))
+            return
+        }
+        parkourer.gotoCheckpoint(course)
     }
 
 }
